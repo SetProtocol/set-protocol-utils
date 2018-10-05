ee93c9870a7b026e3b05fb6af9d25ec0010113d7
Alexander Soong <alexsoong91@gmail.com>
2018-10-05 01:35:24 -0700

    Add method for generating rebalancing set token call data

--- src/index.ts
+++ src/index.ts
@@ -34,6 +34,9 @@ import {
   hashOrderHex,
 } from './orders';
 import {
+  generateRebalancingSetTokenCallData
+} from './rebalancing';
+import {
   parseSignatureHexAsRSV,
   signMessage,
 } from './signing';
@@ -112,6 +115,28 @@ export class SetProtocolUtils {
    */
   public static generateSalt(): BigNumber {
     return generateSalt();
+  }
+
+  /**
+   * Function for clarifying the additional call data parameters that need to be sent to Core when creating
+   * a new rebalancing set token
+   *
+   * @param  managerAddress      Address of the manager to manage the rebalancing
+   * @param  proposalPeriod      Time the participants of the Set can withdraw from a rebalance
+   *                               once a new Set has been proposed
+   * @param  rebalanceInterval   Time between when the manager can initiate another rebalance
+   * @return                     String representing call data to send to Core contracts
+   */
+  public static generateRebalancingSetTokenCallData(
+    managerAddress: Address,
+    proposalPeriod: BigNumber,
+    rebalanceInterval: BigNumber,
+  ): string {
+    return generateRebalancingSetTokenCallData(
+      managerAddress,
+      proposalPeriod,
+      rebalanceInterval,
+    );
   }
 
   /**
