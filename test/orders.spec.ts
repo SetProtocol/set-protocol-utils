c2b22ec2315d9d364cb26cde41c0a22be2b2f77a
felix2feng <felix2feng@gmail.com>
2018-11-07 19:22:38 -0800

    Test generate order hash for consistency

--- test/orders.spec.ts
+++ test/orders.spec.ts
@@ -1,9 +1,10 @@
 
 import * as chai from 'chai';
+import { BigNumber } from '../src/bignumber';
 
 import {
   IssuanceOrder,
-} from './types';
+} from '../src/types';
 import { generateEIP712IssuanceOrderHash, generateOrderHash } from '../src/orders';
 
 const { expect } = chai;
@@ -27,21 +28,31 @@ describe('Orders', () => {
 		let subjectIssuanceOrder: IssuanceOrder;
 
 		function subject(): string {
-      return generateOrderHash();
+      return generateOrderHash(subjectIssuanceOrder);
     }
 
-    describe('', () => {
-    	beforeEach(() => {
+  	it('should generate the correct issuance order hash', () => {
+  		subjectIssuanceOrder = {
+  			setAddress: "0xA8dDa8d7F5310E4A9E24F8eBA77E091Ac264f872",
+  			makerAddress: "0xE834EC434DABA538cd1b9Fe1582052B880BD7e63",
+  			makerToken: "0x06cEf8E666768cC40Cc78CF93d9611019dDcB628",
+  			relayerAddress: "0x78dc5D2D739606d31509C31d654056A45185ECb6",
+  			relayerToken: "0x4404ac8bd8F9618D27Ad2f1485AA1B2cFD82482D",
+  			quantity: new BigNumber('4000000000000000000'),
+  			makerTokenAmount:  new BigNumber("10000000000000000000"),
+  			expiration: new BigNumber("1541647310"),
+  			makerRelayerFee: new BigNumber("1000000000000000000"),
+  			takerRelayerFee: new BigNumber("2000000000000000000"),
+  			salt: new BigNumber("67973443484056420122959491841293512889323885373764273392266387061081721811775"),
+  			requiredComponents: ["0x06cEf8E666768cC40Cc78CF93d9611019dDcB628","0x4404ac8bd8F9618D27Ad2f1485AA1B2cFD82482D"],
+  			requiredComponentAmounts: [new BigNumber("2000000000000000000"), new BigNumber("2000000000000000000")]
+  		};
 
-    	});
+  		const issuanceOrderHash = subject();
 
-    	it('should generate the correct issuance order hash', () => {
-    		const issuanceOrderHash = subject();
+  		const expectedHash = '0x3c3749f295b9df7005e51bdb113516a4cf146a2319a5da9612614915d9268043';
 
-    		const expectedHash = '';
-
-    	});
-    });
-
+  		expect(issuanceOrderHash).to.equal(expectedHash);
+  	});
 	});
 });
