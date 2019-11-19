defb99dd9195353235df3d925d2be2aafb3853f4
Felix Feng <felix2feng@gmail.com>
2019-11-19 13:05:36 -0800

    Add fees to RebalancingSetTokeV2 Calldata (#83)
    * Add fees to calldata
    
    * Remove exit fee
    
    * REmove exit fee

--- src/index.ts
+++ src/index.ts
@@ -149,25 +149,34 @@ export class SetProtocolUtils {
    * Function for clarifying the additional call data parameters that need to be sent to Core when creating
    * a new rebalancing set token
    *
-   * @param  managerAddress      Address of the manager to manage the rebalancing
-   * @param  liquidatorAddress   Address of the contract that handles trade execution
-   * @param  rebalanceInterval   Time between when the manager can initiate another rebalance
+   * @param  managerAddress           Address of the manager to manage the rebalancing
+   * @param  liquidatorAddress        Address of the contract that handles trade execution
+   * @param  recipientAddress         Address of recipient of fees
+   * @param  rebalanceInterval        Time between when the manager can initiate another rebalance
    * @param  lastRebalanceTimestamp   Customized time in seconds of the last rebalance
+   * @param  entryFee                 Mint fee in scaled value (10e18 value)
+   * @param  rebalanceFee             Rebalance fee in scaled value
    * @return                     String representing call data to send to Core contracts
    */
   public static generateRebalancingSetTokenV2CallData(
     managerAddress: Address,
     liquidatorAddress: Address,
+    feeRecipient: Address,
     rebalanceInterval: BigNumber,
     failRebalancePeriod: BigNumber,
     lastRebalanceTimestamp: BigNumber,
+    entryFee: BigNumber,
+    rebalanceFee: BigNumber,
   ): string {
     return generateRebalancingSetTokenV2CallData(
       managerAddress,
       liquidatorAddress,
+      feeRecipient,
       rebalanceInterval,
       failRebalancePeriod,
       lastRebalanceTimestamp,
+      entryFee,
+      rebalanceFee,
     );
   }
 
